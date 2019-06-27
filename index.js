38350717606771d2f02365cd3c41ccae44d87e55
Joanna Zhou <joanna.chow.jc.jc@gmail.com>
2019-06-27 13:07:25 -0400

    fixed problems in game logic

--- index.js
+++ index.js
@@ -110,6 +110,7 @@ function removeUser(id){
 function resetUser(id){
 	const userIndex = users.findIndex(findUserIndex, id);
 	users[userIndex].OpponentId = "";
+	users[userIndex].Roomname = "";
 	users[userIndex].Selection = "charge";
 	users[userIndex].Charge = 0;
 	users[userIndex].Ready = false;
@@ -142,11 +143,14 @@ io.on('connection', function(socket){
 
 	socket.on('back_to_lobby', function(){
 		const userIndex = users.findIndex(findUserIndex, socket.id);
+		const opponentUser = users[userIndex].OpponentId;
 		const roomname = users[userIndex].Roomname;
 		checkOut(socket.id);
 		resetUser(socket.id);
+		resetUser(opponentUser);
 		socket.leave(roomname);
 		io.to(socket.id).emit('back_to_lobby', `Left room ${roomname}.`);
+		io.to(opponentUser).emit('back_to_lobby', `Opponent player left room.`);
 		console.log(`user ${socket.id} left room ${roomname}.`);
 	});
 
@@ -190,8 +194,7 @@ io.on('connection', function(socket){
 
 		if(isFull(roomname) && allReady(roomname)){
 			// 2 players are ready, start game
-			socket.to(roomname).emit('start');
-			io.to(socket.id).emit('start');
+			io.in(roomname).emit('start');
 			console.log(`In room ${roomname}, user ${socket.id} started the game.`);
 		}
 		else{
@@ -225,17 +228,14 @@ io.on('connection', function(socket){
 			switch(users[p1Index].Selection){
 				case "charge":
 					users[p1Index].Charge++;
-					if(users[p1Index].Charge >= 1) io.to(socket.id).emit('enable_hit');
 
 					if(users[p2Index].Selection == "charge"){
 						users[p2Index].Charge++;
-						if(users[p2Index].Charge >= 1) io.to(opponentId).emit('enable_hit');
 						io.to(socket.id).emit('continue', `Opponent charged, you are safe.`, users[p1Index].Charge);
 						io.to(opponentId).emit('continue', `Opponent charged, you are safe.`, users[p2Index].Charge);
 					}
 					else if(users[p2Index].Selection == "hit"){
 						users[p2Index].Charge--;
-						if(users[p2Index].Charge == 0) io.to(opponentId).emit('disable_hit');
 						users[p1Index].Ready = false;
 						users[p2Index].Ready = false;
 						io.to(socket.id).emit('end', `Opponent hit, you lose.`, users[p1Index].Charge);
@@ -253,7 +253,6 @@ io.on('connection', function(socket){
 
 					if(users[p2Index].Selection == "charge"){
 						users[p2Index].Charge++;
-						if(users[p2Index].Charge >= 1) io.to(opponentId).emit('enable_hit');
 						users[p1Index].Ready = false;
 						users[p2Index].Ready = false;
 						io.to(socket.id).emit('end', `Opponent charged, you win.`, users[p1Index].Charge);
@@ -261,7 +260,6 @@ io.on('connection', function(socket){
 					}
 					else if(users[p2Index].Selection == "hit"){
 						users[p2Index].Charge--;
-						if(users[p2Index].Charge == 0) io.to(opponentId).emit('disable_hit');
 						io.to(socket.id).emit('continue', `Opponent hit, you are even.`, users[p1Index].Charge);
 						io.to(opponentId).emit('continue', `Opponent hit, you are even.`, users[p2Index].Charge);
 						
@@ -276,13 +274,11 @@ io.on('connection', function(socket){
 					// case 'defend'
 					if(users[p2Index].Selection == "charge"){
 						users[p2Index].Charge++;
-						if(users[p2Index].Charge >= 1) io.to(opponentId).emit('enable_hit');
 						io.to(socket.id).emit('continue', `Opponent charged, you are safe.`, users[p1Index].Charge);
 						io.to(opponentId).emit('continue', `Opponent defended, you are safe.`, users[p2Index].Charge);
 					}
 					else if(users[p2Index].Selection == "hit"){
 						users[p2Index].Charge--;
-						if(users[p2Index].Charge == 0) io.to(opponentId).emit('disable_hit');
 						io.to(socket.id).emit('continue', `Opponent hit, you are safe.`, users[p1Index].Charge);
 						io.to(opponentId).emit('continue', `Opponent defended, you are even.`, users[p2Index].Charge);
 						
@@ -295,10 +291,10 @@ io.on('connection', function(socket){
 					break;
 			}
 
-			//reset selection
-			users[p1Index].Selection = "charge";
-			users[p2Index].Selection = "charge";
-
+			if(users[p1Index].Charge >= 1) io.to(socket.id).emit('enable_hit');
+			else io.to(socket.id).emit('disable_hit');
+			if(users[p2Index].Charge >= 1) io.to(opponentId).emit('enable_hit');
+			else io.to(opponentId).emit('disable_hit');
 		}	
 	});
 });
